3a297c95037cf0fa9465a1c1b17b3364032cde57
Yashraj Bhargava <yashrajbhargava@Yashrajs-MacBook-Pro.local>
2024-01-11 01:29:42 +0530

    .

--- firstProgram.java
+++ firstProgram.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class FirstProgram{
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        try{
+            int a1=sc.nextInt();
+        int a2=sc.nextByte();
+        System.out.println(a1);
         System.out.println("First  Program!");
+        System.out.println(a2);
         String s1="hi there";
         String s2="how are you?";
         System.out.println(s1+","+s2);
@@ -8,5 +15,10 @@ public class FirstProgram{
         int[] b=a;
         a[1]=9;
         System.out.println(a[1]+""+b[1]);
+        }
+        finally{
+            sc.close();
+        }
+        
     }
 }
