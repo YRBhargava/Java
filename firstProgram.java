c1cb6a3a4338158c0637ef9db7980adb4cd09ea8
Yashraj Bhargava <yashrajbhargava@Yashrajs-MacBook-Pro.local>
2024-01-11 00:33:17 +0530

    .

--- firstProgram.java
+++ firstProgram.java
@@ -1,5 +1,12 @@
 public class FirstProgram{
     public static void main(String[] args) {
         System.out.println("First  Program!");
+        String s1="hi there";
+        String s2="how are you?";
+        System.out.println(s1+","+s2);
+        int[] a={1,2,3};
+        int[] b=a;
+        a[1]=9;
+        System.out.println(a[1]+""+b[1]);
     }
 }
