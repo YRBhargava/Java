ba035a6472e88590982f4879f0e376dcbd472802
Yashraj Bhargava <yashrajbhargava@Yashrajs-MacBook-Pro.local>
2024-01-11 15:20:23 +0530

    .

---
+++ Strings.java
@@ -0,0 +1,8 @@
+public class Strings {
+    public static void main(String[] args) {
+        String s=new String("YASHRAJ");
+        int a=6;
+        float b=1.234f;
+        System.out.printf("a and b are %d and %f accordingly",a,b);
+    }
+}
